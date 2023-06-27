fd8aa590e8164641f75fe7c5382cc93e4127da4b
RealHenryMartinez <henrymtz0390@gmail.com>
2023-06-26 21:05:42 -0700

    lightbulb

---
+++ app.js
@@ -0,0 +1,9 @@
+const img = document.getElementById("img")
+
+function On() {
+    img.src = "https://www.lightbulbs.com/catalog/product_line_photos/light_bulbs_beauty.jpg?123456"
+}
+
+function Off() {
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Gluehlampe_01_KMJ.png/640px-Gluehlampe_01_KMJ.png"
+}
